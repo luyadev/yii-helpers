33ab2925982bf317fbe3972e17f8d32c0e159985
Basil Suter <git@nadar.io>
2020-11-01 21:40:30 +0000

    add server infos

--- tests/HelpersTestCase.php
+++ tests/HelpersTestCase.php
@@ -5,6 +5,14 @@ namespace luya\helpers\tests;
 use luya\base\Boot;
 use luya\testsuite\cases\BaseTestSuite;
 
+$_SERVER['HTTP_HOST'] = 'localhost';
+$_SERVER['SERVER_NAME'] = 'localhost';
+$_SERVER['DOCUMENT_ROOT'] = '/var/www';
+$_SERVER['REQUEST_URI'] = '/luya/envs/dev/public_html/';
+$_SERVER['SCRIPT_NAME'] = '/luya/envs/dev/public_html/index.php';
+$_SERVER['PHP_SELF'] = '/luya/envs/dev/public_html/index.php';
+$_SERVER['SCRIPT_FILENAME'] = '/var/www/luya/envs/dev/public_html/index.php';
+
 class HelpersTestCase extends BaseTestSuite
 {
     public function getConfigArray()
