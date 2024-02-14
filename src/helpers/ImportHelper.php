1d6f013b295959abb9236105236165f179ebcb39
Nadar <git@nadar.io>
2024-02-14 14:13:59 +0000

    Add ImportHelper::csvFromResource() method to import CSV from a resource object

--- src/helpers/ImportHelper.php
+++ src/helpers/ImportHelper.php
@@ -12,34 +12,10 @@ use Yii;
  */
 class ImportHelper
 {
-    /**
-     * Import a CSV from a string or filename and return array.
-     *
-     * The filename can be either a resource from `fopen()` or a string containing the CSV data. Filenames will be wrapped through {{`Yii::getAlias()`}} method.
-     *
-     * @param string $filename Can be either a filename which is parsed by {{luya\yii\helpers\FileHelper::getFileContent()}} or a string with the contained CSV data.
-     * @param array $options Provide options to the CSV
-     * + `removeHeader`: boolean, Whether the import CSV contains a header in the first row to skip or not. Default value is false.
-     * + `delimiter`: string, The delimiter which is used to explode the columns. Default value is `,`.
-     * + `enclosure`: string, The enclosure which is used between the columns. Default value is `"`.
-     * + `fields`: array, An array with filenames (based on the array header if any, or position) which should be parsed into the final export.
-     * ```php
-     * 'fields' => ['firstname', 'lastname'] // will only parse those fields based on table header (row 0)
-     * 'fields' => [0,1,3] // will only parse fields by those positions if no table header is present. Positions starts at 0.
-     * ```
-     * @return array an array with the CSV data.
-     */
-    public static function csv($filename, array $options = [])
+    public static function csvFromResource($resource, array $options = []) : array
     {
-        $filename = Yii::getAlias($filename);
-
-        // check if a given file name is provided or a CSV based on the content
-        if (FileHelper::getFileInfo($filename)->extension) {
-            $resource = fopen($filename, 'r');
-        } else {
-            $resource = fopen('php://memory', 'rw');
-            fwrite($resource, $filename);
-            rewind($resource);
+        if (!is_resource($resource)) {
+            throw new \InvalidArgumentException("The provided resource is not a valid resource.");
         }
         $data = [];
         while (($row = fgetcsv($resource, 0, ArrayHelper::getValue($options, 'delimiter', ','), ArrayHelper::getValue($options, 'enclosure', '"'))) !== false) {
@@ -73,5 +49,38 @@ class ImportHelper
         }
 
         return $data;
+    }
+
+    /**
+     * Import a CSV from a string or filename and return array.
+     *
+     * The filename can be either a resource from `fopen()` or a string containing the CSV data. Filenames will be wrapped through {{`Yii::getAlias()`}} method.
+     *
+     * @param string $filename Can be either a filename which is parsed by {{luya\yii\helpers\FileHelper::getFileContent()}} or a string with the contained CSV data.
+     * @param array $options Provide options to the CSV
+     * + `removeHeader`: boolean, Whether the import CSV contains a header in the first row to skip or not. Default value is false.
+     * + `delimiter`: string, The delimiter which is used to explode the columns. Default value is `,`.
+     * + `enclosure`: string, The enclosure which is used between the columns. Default value is `"`.
+     * + `fields`: array, An array with filenames (based on the array header if any, or position) which should be parsed into the final export.
+     * ```php
+     * 'fields' => ['firstname', 'lastname'] // will only parse those fields based on table header (row 0)
+     * 'fields' => [0,1,3] // will only parse fields by those positions if no table header is present. Positions starts at 0.
+     * ```
+     * @return array an array with the CSV data.
+     */
+    public static function csv($filename, array $options = [])
+    {
+        $filename = Yii::getAlias($filename);
+
+        // check if a given file name is provided or a CSV based on the content
+        if (FileHelper::getFileInfo($filename)->extension) {
+            $resource = fopen($filename, 'r');
+        } else {
+            $resource = fopen('php://memory', 'rw');
+            fwrite($resource, $filename);
+            rewind($resource);
+        }
+        
+        return self::csvFromResource($resource, $options);
     }
 }
